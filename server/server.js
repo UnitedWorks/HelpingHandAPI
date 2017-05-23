53ec2bd37952c221f7022cdce7957d71c3ad99dd
Mark Hansen <x@markthemark.com>
2017-05-23 17:10:34 -0400

    Think env is processed wrong

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use('/quests', require('./quests/routes'));
 app.use('/users', require('./users/routes'));
 app.use('/s3', s3Router({
-  bucket: process.env.AWS_S3_BUCKET,
+  bucket: 'helping.community/videos',
   ACL: 'public-read'
 }))
 
