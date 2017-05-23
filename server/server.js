72bb0cc2c7d28f2fbe04437f0469354d504f44a7
Mark Hansen <x@markthemark.com>
2017-05-23 15:19:54 -0400

    Accept videos for s3 upload

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
+import s3Router from './s3';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,6 +17,10 @@ app.use(cors());
 // API Routes
 app.use('/quests', require('./quests/routes'));
 app.use('/users', require('./users/routes'));
+app.use('/s3', s3Router({
+  bucket: process.env.AWS_S3_BUCKET,
+  ACL: 'public-read'
+}))
 
 // Start Server
 app.listen(port, () => {
